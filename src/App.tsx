d43f5ce804182eda448488e5c067546016e34eed
Thea <142231299+QueenWoofie@users.noreply.github.com>
2025-11-17 07:48:00 +0100

    Added Product Details Page + Seart Sort Function
    - Added page for details page
    - Adjusted Home page for sort and search function
    - Added route for additional page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { ContactPage } from './pages/ContactPage';
 import { CheckoutSuccessPage } from './pages/CheckoutSuccessPage';
+import { ProductDetailsPage } from './pages/ProductDetailsPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/checkout-success" element={<CheckoutSuccessPage />} />
+        <Route path="/product/:id" element={<ProductDetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
